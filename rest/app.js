57a33020a5827dd96078162c54a93bd7eca8ce83
Mateus Cesar <mateuscesarglima@gmail.com>
2022-11-24 09:35:00 -0300

    ascan last challenge implementation.

---
+++ rest/app.js
@@ -0,0 +1,77 @@
+const AWS = require("aws-sdk");
+const uuid = require("uuid");
+const dynamo = new AWS.DynamoDB.DocumentClient();
+let response;
+
+exports.FetchAllUsers = async (event, context) => {
+  const data = await dynamo
+    .scan({
+      TableName: "usersDB",
+    })
+    .promise();
+  response = {
+    statusCode: 200,
+    body: JSON.stringify({ users: data.Items }),
+  };
+  return response;
+};
+
+exports.CreateUser = async (event, context) => {
+  const { username, email, password } = JSON.parse(event.body);
+  await dynamo
+    .put({
+      TableName: "usersDB",
+      Item: {
+        id: uuid.v4(),
+        username,
+        email,
+        password,
+      },
+    })
+    .promise();
+  response = {
+    statusCode: 200,
+    body: JSON.stringify({ message: "User Created successfuly" }),
+  };
+  return response;
+};
+
+exports.DeleteUser = async (event, context) => {
+  await dynamo
+    .delete({
+      TableName: "usersDB",
+      Key: {
+        id: event.pathParameters.id,
+      },
+    })
+    .promise();
+  response = {
+    statusCode: 200,
+    body: JSON.stringify({ message: "User is Deleted" }),
+  };
+  return response;
+};
+
+exports.UpdateUser = async (event, context) => {
+  const Item = JSON.parse(event.body);
+  await dynamo
+    .update({
+      TableName: "usersDB",
+      Key: {
+        id: event.pathParameters.id,
+      },
+      UpdateExpression: "set username= :u, email= :e, password= :p",
+      ExpressionAttributeValues: {
+        ":u": Item.username,
+        ":e": Item.email,
+        ":p": Item.password,
+      },
+      Returnvalues: "UPDATED_NEW",
+    })
+    .promise();
+  response = {
+    statusCode: 200,
+    body: JSON.stringify({ message: "User is Updated" }),
+  };
+  return response;
+};
